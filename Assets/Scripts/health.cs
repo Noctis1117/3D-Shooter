32ad15283c1598a94935e5c4b0f6923dee64f11d
lthompson23 <lthompson@ltu.edu>
2023-11-30 17:35:45 -0500

    Tweaked enemy slightly

--- Assets/Scripts/health.cs
+++ Assets/Scripts/health.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,15 +18,7 @@ public class health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            TakeDamage(20);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Heal(20);
-        }
+       
     }
 
     public void TakeDamage(float damage)
